209663d7ccab9fa2ee8db5e1cbb7f57892cf73b1
irregular25 <159280012+irregular25@users.noreply.github.com>
2024-03-12 16:05:04 +0100

    Update client.py

--- client.py
+++ client.py
@@ -4,22 +4,20 @@ import sys
 import getopt
 
 from pylsl import StreamInlet, resolve_stream
-import server
 import nnmodel
 
 import mne
-from mne.decoding import CSP
+from mne.decoding import CSP, Vectorizer, FilterEstimator, Scaler, cross_val_multiscore
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import ShuffleSplit, cross_val_score
 from sklearn.metrics import make_scorer, f1_score
 from sklearn.pipeline import Pipeline
-
 from sklearn.svm import SVC
-from sklearn.preprocessing import StandardScaler
-from mne.decoding import Vectorizer, FilterEstimator
+
 
 def update_raw(sample, r):
     ''' concatenate samples as they arrive in a list'''
@@ -30,12 +28,11 @@ def update_raw(sample, r):
 
 
 def main(argv):
-    subject= 1
     min_train = 10
     mod = 'svm'
-    help_string = 'client.py -m <model> -s <subject>'
-    try:
-        opts, args = getopt.getopt(argv, "h:m:s:t", longopts=["model=", "subject=", "min_train"])
+    help_string = 'client.py -m <model> -t <min_train>'
+    '''try:
+        opts, args = getopt.getopt(argv, "h:m:t", longopts=["model=", "min_train"])
     except getopt.GetoptError:
         print(help_string)
         sys.exit(2)
@@ -43,12 +40,10 @@ def main(argv):
         if opt == '-h':
             print(help_string)
             sys.exit()
-        elif opt in ("-s", "--subject"):
-            subject = int(arg)
         elif opt in ("-t", "--min_train"):
             min_train = float(arg)
         elif opt in ("-m", "--model"):
-            mod = str(arg)
+            mod = str(arg)'''
 
     # Look for Streams
     print("looking for an EEG stream...")
@@ -82,7 +77,7 @@ def main(argv):
 
     try:
         while received_samples < desired_samples :
-            # Receive EEG data and Markers
+            ### Receive EEG data and Markers ###
             sample, timestamp = eeg_inlet.pull_sample()
             event_markers, event_timestamp = event_inlet.pull_sample(timeout=0)
         
@@ -104,46 +99,45 @@ def main(argv):
             if (received_samples % 1000 == 0) :
                 print(f"Received EEG data ({received_samples}/{desired_samples})")
         
-            ## Decoding
+            ############################## Decoding #################################
             if (len(received_events) >= min_train) & (received_samples % 5000 == 0):
                 received_info = mne.create_info(channel_names, sfreq= sample_r, ch_types='eeg')
                 received_raw = mne.io.RawArray(received_eeg, received_info, first_sample)
                 
-                # Epochs
+                ## Epochs
                 print(received_events)
                 # Pick EEG channels, exclude bads
                 picks = mne.pick_types(received_info, eeg=True, meg=False, misc=False, exclude='bads')
                 epochs = mne.Epochs(received_raw, received_events, event_id=[2,3], tmin=-0.5, tmax=2.5,baseline=None,preload=True,picks=picks)
-                                    #,reject=dict(eeg=150e-6))
-                X = epochs.get_data(copy=False)  # EEG signals: n_epochs, n_eeg_channels, n_times
-                y = epochs.events[:, 2]  # target: 
+                                    #,reject=dict(eeg=150e-6)) # rejection threshold too high
+                # EEG signals: n_epochs, n_eeg_channels, n_times
+                epochs_data = epochs.get_data(copy=False)  
+                # labels 0 or 1: hands vs feet
+                labels = epochs.events[:, -1] - 2 
+
+                ### Preprocessing steps ###
                 # Apply band-pass filter
                 filt = FilterEstimator(epochs.info, 7.0, 30, fir_design='firwin')
-                scaler = StandardScaler()
-                vectorizer = Vectorizer()
-                # labels 0 or 1:
-                labels = epochs.events[:, -1] - 2
+                # standardize the data based on channel scales (different from scikit-learn)
+                scaler = Scaler(epochs.info)
+                # 2D format for feeding the network
+                vectorizer = Vectorizer()                
 
-                # CSP
+                # CSP: Common Space Pattern
                 csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
-
-                # SVM model
-                svm = SVC(kernel='rbf', gamma=0.7, C = 1.0)
-
-                classifier0 = Pipeline([('filter', filt), ('vector', vectorizer),
-                              ('scaler', scaler), ('svm', svm)])
-                classifiersvm = Pipeline([('filter', filt), ('csp', csp), ('svm', svm)])
                 
-                # Cross-Validation
-                epochs_data = epochs.get_data(copy=False) # EEG signals: n_epochs, n_eeg_channels, n_times
+                ## Cross-Validation
                 cv = ShuffleSplit(5, test_size=0.2, random_state=42)
                 X = epochs_data
                 y = labels
 
                 if mod == 'svm':
-                    
+                    # SVM model
+                    svm = SVC(kernel='rbf', gamma=0.7, C = 1.0)
+                    # Preprocessing : band-pass filter and csp (before svm)
+                    classifiersvm = Pipeline([('filter', filt), ('csp', csp), ('svm', svm)])
                     scores_t = cross_val_score(classifiersvm, X, y, scoring=make_scorer(f1_score, average='weighted'), cv=cv, n_jobs=1)
-                    print(f"F1 Scores ({scores_t})")
+                    print(f" SVM F1 Scores ({scores_t})")
                     std_scores_svm.append(scores_t.std())
                     scores_svm.append(scores_t.mean())
                     iter.append(len(received_events))
@@ -153,15 +147,21 @@ def main(argv):
                     ax.set_xlabel('Epochs (events)')
                     ax.set_ylabel('Classification f1 score')
                     
-                    plt.plot(iter[-2:], scores_svm[-2:], '-x', color='b',label="Classif. f1 score")
-                    ax.plot(iter[-1], scores_svm[-1])
+                    plt.plot(iter, scores_svm, '-x', color='b',label="Classif. f1 score")
+                    ax.plot(iter, scores_svm)
                     hyp_limits = (np.asarray(scores_svm) - np.asarray(std_scores_svm),
                     np.asarray(scores_svm) + np.asarray(std_scores_svm))
                     fill = plt.fill_between(iter, hyp_limits[0], y2=hyp_limits[1], color='b', alpha=0.5)
                     plt.pause(0.01)
                     plt.draw()
-                    fill.remove()  # Remove old fill area
 
+                if mod == 'nn':
+                    # NN model
+                    model = nnmodel.Network().to(device)
+                    # Preprocessing standard
+                    classifier_nn = Pipeline([('filter', filt), ('vector', vectorizer),('scaler', scaler), ('nn', model)])
+                    scores_t2 = cross_val_multiscore(classifier_nn, X, y, cv=cv, n_jobs=None, scoring=make_scorer(f1_score, average='weighted'))
+                    print(f" NN F1 Scores ({scores_t2})")
 
         # Final figure
         plt.fill_between(iter, hyp_limits[0], y2=hyp_limits[1], color='b', alpha=0.5)
